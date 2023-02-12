8d8ede83303604971cf5c36d4d248fc72ead599b
crispindeity <shoy1415@gmail.com>
2023-02-12 22:32:22 +0900

    :sparkles: 서블릿 등록

--- src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
+++ src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
@@ -4,11 +4,25 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class HellobootApplication {
 
     public static void main(String[] args) {
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
-		webServer.start();
-	}
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("hello", new HttpServlet() {
+			@Override
+			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+				resp.setStatus(200);
+				resp.setHeader("Content-Type", "text/plain");
+				resp.getWriter().println("Hello Servlet");
+			}
+		}).addMapping("/hello"));
+        webServer.start();
+    }
 }
