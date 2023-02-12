b5dfdca01360224f1291436a63532e8f74c97ed8
crispindeity <shoy1415@gmail.com>
2023-02-12 22:55:10 +0900

    :sparkles: Hello Controller 매핑및 바인딩 처리

--- src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
+++ src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
@@ -19,23 +19,25 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet("front-controller", new HttpServlet() {
-			@Override
-			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-					String name = req.getParameter("name");
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
+			servletContext.addServlet("front-controller", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+						String ret = helloController.hello(name);
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					} else if (req.getRequestURI().equals("/users")) {
+						//
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-				else if (req.getRequestURI().equals("/users")) {
-					//
-				}
-				else {
-					resp.setStatus(HttpStatus.NOT_FOUND.value());
-				}
-			}
-		}).addMapping("/*"));
+			}).addMapping("/*");
+		});
         webServer.start();
     }
 }
