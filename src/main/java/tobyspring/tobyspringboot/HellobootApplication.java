67f738b66ac986af63cc0f1951cce687cd27deb7
crispindeity <shoy1415@gmail.com>
2023-03-02 22:55:58 +0900

    :sparkles: @Component 스캔

--- src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
+++ src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
@@ -3,12 +3,13 @@ package tobyspring.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class HellobootApplication {
 
     public static void main(String[] args) {
@@ -26,15 +27,5 @@ public class HellobootApplication {
         };
         applicationContext.register(HellobootApplication.class);
         applicationContext.refresh();
-    }
-
-    @Bean
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
-    }
-
-    @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
     }
 }
