2ba4745fe5157395adfe3882aa42bd653a6e2a10
crispindeity <shoy1415@gmail.com>
2023-03-20 23:24:18 +0900

    :sparkles: 자동 구성 정보 파일 분리

--- src/main/java/tobyspring/tobyspringboot/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/tobyspringboot/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,34 @@
 package tobyspring.tobyspringboot.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobyspring.tobyspringboot.config.autoconfig.TomcatWebServerConfig",
-                "tobyspring.tobyspringboot.config.autoconfig.TomcatWebServerConfig"
-        };
+        ArrayList<String> autoConfigs = new ArrayList<>();
+        /**
+         * Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+         * return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+         */
+
+//        for (String candidate : ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+//            autoConfigs.add(candidate);
+//        }
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+//        return autoConfigs.toArray(new String[0]);
+//        return autoConfigs.toArray(String[]::new);
+        return Arrays.copyOf(autoConfigs.toArray(), autoConfigs.size(), String[].class);
     }
 }
