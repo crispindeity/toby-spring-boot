a4477530d2698ad6dbdb1230ed5777d3d2f7c720
crispindeity <shoy1415@gmail.com>
2023-03-20 22:29:52 +0900

    :sparkles: 합성 애노테이션 적용

--- src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
+++ src/main/java/tobyspring/tobyspringboot/HellobootApplication.java
@@ -1,28 +1,11 @@
 package tobyspring.tobyspringboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class HellobootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
-    }
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
     }
 }
