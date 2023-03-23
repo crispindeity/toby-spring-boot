b4c61c643621c11b5698908023bba63ed13c2349
crispindeity <shoy1415@gmail.com>
2023-03-23 23:17:05 +0900

    :heavy_plus_sign: 스타와 Jetty 서버 구성 추가

--- src/main/java/tobyspring/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
@@ -1,15 +1,15 @@
 package tobyspring.tobyspringboot.config.autoconfig;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 import tobyspring.tobyspringboot.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean(name = "tomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean(name = "jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
