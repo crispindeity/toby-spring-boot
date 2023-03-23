f9350c9a42da793775d0e928c0fd8eaa3f6b1e82
crispindeity <shoy1415@gmail.com>
2023-03-23 23:36:38 +0900

    :sparkles: @Conditional과 Condition

--- src/main/java/tobyspring/tobyspringboot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/tobyspringboot/config/autoconfig/TomcatWebServerConfig.java
@@ -1,15 +1,29 @@
 package tobyspring.tobyspringboot.config.autoconfig;
 
+import static tobyspring.tobyspringboot.config.autoconfig.TomcatWebServerConfig.TomcatCondition;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import tobyspring.tobyspringboot.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatCondition.class)
 public class TomcatWebServerConfig {
     @Bean(name = "tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
